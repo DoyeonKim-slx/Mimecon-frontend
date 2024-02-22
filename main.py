82b43f0a7c0a2bbe527092c8c94e6a240a533418
hanbit.kang <hbkang.kang@saltlux.com>
2024-02-22 19:06:50 +0900

    부위별 독립적 작동 기능 추가

--- main.py
+++ main.py
@@ -4,13 +4,16 @@ import cv2
 
 img0 = cv2.imread("sample/prev-0-w.png")
 pts0 = np.loadtxt("sample/prev-0_face_open_mouth.txt")[:, :2]
-pts = np.copy(pts0)
-h,w,c = img0.shape
-
-hold = False
-mode = 'none'
-ox, oy = 0, 0
-dx, dy, dw, dh = 0, 0, 0, 0
+LANDMARK_INFO = {
+    'eyebrow_l':[17,22],
+    'eyebrow_r':[22,27],
+    'nose':     [27,36],
+    'eye_left': [36,42],
+    'eye_right':[42,48],
+    'mouth_in': [60,68], # 다른 부위 가리기 방지
+    'mouth_out':[48,60],
+    'jaw':      [ 0,17], # 다른 부위 가리기 방지
+}
 
 def get_bbox(face_pts):
     min_x, min_y = face_pts[:,0].min(), face_pts[:,1].min()
@@ -20,112 +23,171 @@ def get_bbox(face_pts):
 
     return np.array([min_x, min_y, w, h]).astype(int)
 
-face_bbox0 = get_bbox(pts0)
-face_bbox = np.copy(face_bbox0)
+class LandmarkGroup:
+    mode: str = 'inactive'
+    dx: int = 0
+    dy: int = 0
+    dw: int = 0
+    dh: int = 0
 
-def mouse_callback(event, x,y, flags, params):
-    global pts, pts0, face_bbox, face_bbox0
-    global hold, mode, ox, oy, dx, dy, dw, dh
+    def __init__(self, landmarks):
+        self.landmarks = landmarks.copy()
+        self.bbox = get_bbox(landmarks)
+        self.orig_bbox = self.bbox.copy()
+        self.landmarks[:,0] -= self.orig_bbox[0]
+        self.landmarks[:,1] -= self.orig_bbox[1]
+    
+    @property
+    def x(self):
+        return self.bbox[0]
+    @x.setter
+    def x(self, v):
+        self.bbox[0] = v
+    @property
+    def y(self):
+        return self.bbox[1]
+    @x.setter
+    def y(self, v):
+        self.bbox[1] = v
+    @property
+    def w(self):
+        return self.bbox[2]
+    @x.setter
+    def w(self, v):
+        self.bbox[2] = v
+    @property
+    def h(self):
+        return self.bbox[3]
+    @x.setter
+    def h(self, v):
+        self.bbox[3] = v
+    
+    def get_pts(self):
+        sw, sh = (self.bbox[2]+self.dw)/self.orig_bbox[2], (self.bbox[3]+self.dh)/self.orig_bbox[3]
+        landmarks = self.landmarks.copy()
+        landmarks[:, 0] = landmarks[:, 0]*sw + self.bbox[0] + self.dx
+        landmarks[:, 1] = landmarks[:, 1]*sh + self.bbox[1] + self.dy
+        return landmarks
+    
+    def check_active(self, px,py):
+        x,y,w,h = self.bbox
+        for (m, cx,cy) in [
+            ('tl',x,y), ('tm', x+w/2,y), ('tr', x+w,y),
+            ('lm',x,y+h/2), ('rm', x+w,y+h/2),
+            ('bl',x,y+h), ('bm',x+w/2,y+h), ('br',x+w,y+h),
+        ]:
+            if (cx-5<px<cx+5) and (cy-5<py<cy+5):
+                self.mode = m
+                return True
+        if (x < px < x+w) and (y < py < y+h):
+            self.mode = 'drag'
+            return True
+        return False
+
+    def set_delta(self, dx, dy):
+        if self.mode == 'drag':
+            self.dx, self.dy = dx, dy
+        if self.mode == 'tl':
+            self.dx = dx
+            self.dw = -dx
+            self.dy = dy
+            self.dh = -dy
+        if self.mode == 'tm':
+            self.dy = dy
+            self.dh = -dy
+        if self.mode == 'tr':
+            self.dw = dx
+            self.dy = dy
+            self.dh = -dy
+        if self.mode == 'lm':
+            self.dx = dx
+            self.dw = -dx
+        if self.mode == 'rm':
+            self.dw = dx
+        if self.mode == 'bl':
+            self.dx = dx
+            self.dw = -dx
+            self.dh = dy
+        if self.mode == 'bm':
+            self.dh = dy
+        if self.mode == 'br':
+            self.dw = dx
+            self.dh = dy
+
+    def release(self):
+        self.mode = 'inactive'
+        self.bbox[0] += self.dx
+        self.bbox[1] += self.dy
+        self.bbox[2] += self.dw
+        self.bbox[3] += self.dh
+        self.dx = 0
+        self.dy = 0
+        self.dw = 0
+        self.dh = 0
+    
+    def draw(self, img):
+        x,y,w,h = self.bbox[0],self.bbox[1],self.bbox[2],self.bbox[3]
+        x,y = x+self.dx,y+self.dy
+        w,h = w+self.dw,h+self.dh
+        # draw bbox
+        img = cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 1)
+        for (cx,cy) in [
+            (x,y), (x+w/2,y), (x+w,y),
+            (x,y+h/2), (x+w,y+h/2),
+            (x,y+h), (x+w/2,y+h), (x+w,y+h),
+        ]:
+            cx,cy = int(cx),int(cy)
+            img = cv2.rectangle(img, (cx-5,cy-5), (cx+5,cy+5), (0,255,0), 2)
+        
+        # draw landmarks
+        for pt in self.get_pts():
+            px, py = pt[0], pt[1]
+            px, py = int(px), int(py)
+            img = cv2.circle(img, (px,py), 2, (0,0,255), 2)
+        
+        return img
+
+landmark_groups = {}
+for name, (start_i, end_i) in LANDMARK_INFO.items():
+    landmark_groups[name] = LandmarkGroup(pts0[start_i:end_i])
+
+hold = False
+current = 'none'
+ox, oy = 0, 0
+
+def check_active(px,py):
+    global landmark_groups, hold, current, ox, oy
+    for name, group in landmark_groups.items():
+        if group.check_active(px, py):
+            hold = True
+            current = name
+            ox, oy = px, py
+            return
+
+def mouse_callback(event, px,py, flags, params):
+    global landmark_groups
+    global hold, current, ox, oy
 
     if event == cv2.EVENT_LBUTTONDOWN:
-        hold = True
-        ox, oy = x, y
-        set_mode(x,y)
-        print(mode)
+        check_active(px,py)
         return
     if hold and event == cv2.EVENT_MOUSEMOVE:
-        if mode == 'drag':
-            dx, dy = x-ox, y-oy
-        if mode == 'tl':
-            dx = x-ox
-            dw = ox-x
-            dy = y-oy
-            dh = oy-y
-        if mode == 'tm':
-            dy = y-oy
-            dh = oy-y
-        if mode == 'tr':
-            dw = x-ox
-            dy = y-oy
-            dh = oy-y
-        if mode == 'lm':
-            dx = x-ox
-            dw = ox-x
-        if mode == 'rm':
-            dw = x-ox
-        if mode == 'bl':
-            dx = x-ox
-            dw = ox-x
-            dh = y-oy
-        if mode == 'bm':
-            dh = y-oy
-        if mode == 'br':
-            dw = x-ox
-            dh = y-oy
+        dx, dy = px-ox, py-oy
+        landmark_groups[current].set_delta(dx,dy)
         return
-    if event == cv2.EVENT_LBUTTONUP:
+    if hold and event == cv2.EVENT_LBUTTONUP:
+        landmark_groups[current].release()
         hold = False
-
-        face_bbox[0] += dx
-        face_bbox[1] += dy
-        face_bbox[2] += dw
-        face_bbox[3] += dh
-        
-        sw,sh = face_bbox[2]/face_bbox0[2], face_bbox[3]/face_bbox0[3]
-        pts[:,0] = (pts0[:,0] - face_bbox0[0])*sw + face_bbox[0]
-        pts[:,1] = (pts0[:,1] - face_bbox0[1])*sh + face_bbox[1]
-        
-        mode = 'none'
+        current = 'none'
         ox, oy = 0, 0
-        dx, dy, dw, dh = 0, 0, 0, 0
         return
-
-def set_mode(px,py):
-    global mode
-    x,y,w,h = face_bbox
-    for (m, cx,cy) in [
-        ('tl',x,y), ('tm', x+w/2,y), ('tr', x+w,y),
-        ('lm',x,y+h/2), ('rm', x+w,y+h/2),
-        ('bl',x,y+h), ('bm',x+w/2,y+h), ('br',x+w,y+h),
-    ]:
-        if (cx-5<px<cx+5) and (cy-5<py<cy+5):
-            mode = m
-            return
-    if (x < px < x+w) and (y < py < y+h):
-        mode = 'drag'
-        return
-
-def draw_pts(img):
-    global hold, mode, dx, dy, dw, dh
-    global pts, face_bbox
-    x,y,w,h = face_bbox
-    ox,oy,x,y = x,y,x+dx,y+dy
-    ow,oh,w,h = w,h,w+dw,h+dh
-    sw, sh = w/ow, h/oh
-
-    # draw landmarks
-    for pt in pts:
-        px, py = pt[0]-ox, pt[1]-oy
-        px, py = px*sw+dx+ox,py*sh+dy+oy
-        px, py = int(px), int(py)
-        img = cv2.circle(img, (px,py), 2, (0,0,255), 2)
-
-    # draw bbox
-    img = cv2.rectangle(img, (x,y), (x+w,y+h), (0,255,0), 2)
-    for (cx,cy) in [
-        (x,y), (x+w/2,y), (x+w,y),
-        (x,y+h/2), (x+w,y+h/2),
-        (x,y+h), (x+w/2,y+h), (x+w,y+h),
-    ]:
-        cx,cy = int(cx),int(cy)
-        img = cv2.rectangle(img, (cx-5,cy-5), (cx+5,cy+5), (0,255,0), 2)
-    return img
 
 def draw_image():
+    global landmark_groups
     img = np.copy(img0)
-    img = draw_pts(img)
+    for groups in landmark_groups.values():
+        img = groups.draw(img)
     cv2.imshow('app', img)
-
 
 cv2.namedWindow('app')
 cv2.setMouseCallback('app', mouse_callback, {})
@@ -136,4 +198,9 @@ while True:
     if key == ord('q'):
         break
 cv2.destroyAllWindows()
-print(pts)
+
+new_pts = np.zeros_like(pts0)
+for name, groups in landmark_groups.items():
+    (start_i, end_i) = LANDMARK_INFO[name]
+    new_pts[start_i:end_i] = groups.get_pts()
+print(new_pts)
